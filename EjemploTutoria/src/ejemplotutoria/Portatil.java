3161fdaebc4dff0aa98abd7be521e8a6b0ee8ce1
Marjan Celi <marjanazliga@gmail.com>
2019-06-27 19:38:52 -0500

    Programacion Orientada a Objetos

---
+++ EjemploTutoria/src/ejemplotutoria/Portatil.java
@@ -0,0 +1,27 @@
+
+package ejemplotutoria;
+
+public class Portatil {
+    
+    String color;
+    String procesador;
+    
+    public void establecerColor (String c){
+        
+        color = c;
+    }
+    public String obtenerColor(){
+        
+        return color;
+    }
+    
+    public void establecerProcesador (String p){
+        
+        color = p;
+    }
+    public String ObtenerProcesador(){
+        
+        return procesador;
+    }
+    
+}
